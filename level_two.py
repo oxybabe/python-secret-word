3482d544de73584c6080749c03db05cd7e57b882
Elia Partin <arenaselia@gmail.com>
2023-06-13 16:23:20 -0400

    finished level_two.py

--- level_two.py
+++ level_two.py
@@ -1,9 +1,11 @@
 import random
 
-word_bank = ["cat", "design", "cherry", "time", "insure", "harmony", "firefighter", "researcher", "dictionary"]
+word_bank = ["cat", "design", "cherry", "time"]
 print("Welcome to the secret word game!")
 print("Can you guess the secret word?")
-secret_word = random.choices(word_bank, k=1)
+secret_word = random.choice(word_bank)
+secret_word = list(secret_word)
+print(secret_word)
 guess_list = []
 correct = False
 while not correct:
@@ -14,7 +16,7 @@ while not correct:
         print("You guessed the following correct letters: ", guess_list)
     elif guess not in secret_word:
         print("You guessed an incorrect letter!")
-    if guess_list == secret_word:
+    if guess_list == sorted(secret_word):
         print("Correct! You guessed the secret word!") 
         correct = True
         
