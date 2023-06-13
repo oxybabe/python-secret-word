8a18b266756237cb1b84415236d8fc86705acc02
Elia Partin <arenaselia@gmail.com>
2023-06-13 15:32:48 -0400

    fixed level1

--- level_one.py
+++ level_one.py
@@ -1,20 +1,22 @@
 print("Welcome to the secret word game!")
 print("I am thinking of a 3 letter word. Can you guess the word?")
-secret_word = "cat"
+secret_word = ("c", "a", "t")
+secret_word = sorted(secret_word)
 guess_list = []
 correct = False
 while not correct:
     guess = input("Guess a letter between a-z: ")
-    if guess == secret_word:
-        correct = True
-        print("Correct! You guessed the secret word!")
-    elif guess not in secret_word:
-        print("You guessed an incorrect letter!")
-    elif guess in secret_word:
+    guess_list = sorted(guess_list)
+    if guess in secret_word:
         guess_list.append(guess) 
         print("You guessed the following correct letters: ", guess_list)
-    if guess_list == ["c", "a", "t"]:
-        print("Correct! You guessed all the correct letters!")
+    elif guess not in secret_word:
+        print("You guessed an incorrect letter!")
+    if guess_list == secret_word:
+        print("Correct! You guessed the secret word!") 
+        correct = True
+        
+        
         
 
         
--- level_two.py
+++ level_two.py
@@ -1,21 +1,23 @@
 import random
+
+word_bank = ["cat", "design", "cherry", "time", "insure", "harmony", "firefighter", "researcher", "dictionary"]
 print("Welcome to the secret word game!")
-print("I am thinking of a 3 letter word. Can you guess the word?")
-secret_word = "cat"
+print("Can you guess the secret word?")
+secret_word = random.choices(word_bank, k=1)
 guess_list = []
 correct = False
 while not correct:
     guess = input("Guess a letter between a-z: ")
-    if guess == secret_word:
-        correct = True
-        print("Correct! You guessed the secret word!")
-    elif guess not in secret_word:
-        print("You guessed an incorrect letter!")
-    elif guess in secret_word:
+    guess_list = sorted(guess_list)
+    if guess in secret_word:
         guess_list.append(guess) 
         print("You guessed the following correct letters: ", guess_list)
-    if guess_list == ["c", "a", "t"]:
-        print("Correct! You guessed all the correct letters!")
+    elif guess not in secret_word:
+        print("You guessed an incorrect letter!")
+    if guess_list == secret_word:
+        print("Correct! You guessed the secret word!") 
+        correct = True
+        
         
 
 
